c257342d43063d607e8a7cb2942fd9be3a1a8200
faizamahmoud <faiza.mahmoud@email.com>
2022-12-05 14:03:21 -0700

    pushing up

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Routes>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Routes>
   </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

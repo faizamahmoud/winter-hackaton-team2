ebb2f8553f52b1b179b35f507fc5086d7cca29b4
rpeterson87 <oshorhino5053@gmail.com>
2022-12-06 16:23:20 -0500

    Context working

--- src/context/shelterContext.jsx
+++ src/context/shelterContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { useEffect, useContext } from "react";
 
 const ShelterContext = createContext()
 
@@ -13,11 +14,47 @@ const maps = (lat, lan) => {
     setItems((prevState) => [...prevState, {lat, lan}]);
 }
 
+const [rescueGroupData, setRescueGroupData] = useState()
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+       fetch('https://api.rescuegroups.org/v5/public/orgs/search', {
+          method: 'POST',
+          headers: {
+            'Content-Type':'application/vnd.api+json',
+            'Authorization': 'GPKNSuYF'
+          },
+          body: JSON.stringify({
+            data: {
+              'filterRadius':{
+                'postalcode': 90210,
+                'miles': 25
+              }
+            }
+          })
+        })
+        .then(response => response.json())
+        .then(json => setRescueGroupData(json.data)) 
+        .then(() => console.log(rescueGroupData)) 
+      }
+       catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+
+  }, [])
+
     return(
-        <ShelterContext.Provider value={{item:1}}>
+        <ShelterContext.Provider value={{item:1, rescueGroupData}}>
             {children}
         </ShelterContext.Provider>
     )
+}
+
+export const useGlobalContext = () => {
+    return useContext(ShelterContext)
 }
 
 export default ShelterContext
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ShelterProvider } from './context/shelterContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
+    <ShelterProvider>
     <Router>
       <App />
     </Router>
+    </ShelterProvider>
   </React.StrictMode>
 );

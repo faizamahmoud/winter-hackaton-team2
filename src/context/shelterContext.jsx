4ada1261d376311b1eef72ec91b8f10f16b52450
SaidNivek <kevin.vi.dias@gmail.com>
2022-12-07 13:13:15 -0500

    Fix merge syntax errors, no other changes

--- src/context/shelterContext.jsx
+++ src/context/shelterContext.jsx
@@ -11,20 +11,15 @@ export const ShelterProvider = ({children}) => {
 const [item, setItems] = useState([]);
 const [rescueGroupData, setRescueGroupData] = useState()
 
-
 // function flyThis () {map.flyTo({
 //   center: [(Math.random() - 0.5) * 360, (Math.random() - 0.5) * 100],
 //   essential: true // this animation is considered essential with respect to prefers-reduced-motion
 //   })};
 
-
-useEffect(() => {
-     async function fetchData() {
-
 const [postalCode, setPostalCode] = useState()
 const [distance, setDistance] = useState('25')
 const [coords, setCoords] = useState([])
- 
+
   useEffect(() => {
     async function fetchData() {
 
@@ -56,10 +51,6 @@ const [coords, setCoords] = useState([])
     }
   }, [postalCode, distance])
 
-
-
-
-  }, [])
 
   // This useEffect will take the lat and lon from the rescueGroupData, if it exists, and set the coords array to objects with the lat and lon of each shelter.
   // This info will be used with the map, to provide markers for the user

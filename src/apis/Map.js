e8d47b10e9327e2163f9816a9d0150b50c72b069
faizamahmoud <faiza.mahmoud@email.com>
2022-12-07 11:05:24 -0700

    need to pull down updates

--- src/apis/Map.js
+++ src/apis/Map.js
@@ -1,10 +1,13 @@
+
+
 import React, {useRef, useEffect, useState} from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import { useGlobalContext } from '../context/shelterContext';
 
-const T = process.env.REACT_APP_TOKEN
-mapboxgl.accessToken=T
 
+
+// const T = process.env.REACT_APP_TOKEN
+// mapboxgl.accessToken = T
 // 'pk.eyJ1IjoibXZya2xqYW4iLCJhIjoiY2w1bXYzdjIxMDE3cjNjbXR5ejl2bWFveSJ9.KD9v7tW8kslwNGmg-fOjcQ'
 function Map() {
 
@@ -16,36 +19,57 @@ function Map() {
     const [lat, setLat] = useState(37.79);
     const [zoom, setZoom] = useState(10);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom
-      });
-    }, []);
+
+// function Map() {
+
+//   const mapContainer = useRef(null);
+//   const map = useRef(null);
+
+//   const [lng, setLng] = useState(-122.26);
+//   const [lat, setLat] = useState(37.79);
+//   const [zoom, setZoom] = useState(10);
 
 //   useEffect(() => {
-//     if (!map.current) return; // wait for map to initialize
-//     map.current.on('move', () => {
-//     setLng(map.current.getCenter().lng.toFixed(4));
-//     setLat(map.current.getCenter().lat.toFixed(4));
-//     setZoom(map.current.getZoom().toFixed(2));
-//      });
+//     if (map.current) return; // initialize map only once
+//     map.current = new mapboxgl.Map({
+//       container: mapContainer.current,
+//       style: 'mapbox://styles/mapbox/streets-v12',
+//       // center: [lng,lat],
+//       center: [-87.65, 41.84],
+//       // zoom: zoom
+//       zoom: 10
 //     });
-   
 
-  return (
-    <div>
-        <h1>MapS!!</h1>
-        {/* Gives the map with the inital coordinates */}
-        <div ref={mapContainer} className="map-container" />
 
-        {/* not sure if we need the next line, used for showing lat/long on screen */}
-        {/* <div className="sidebar">Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}</div> */}
-    </div>
-  )
+//     // Create default markers
+//     geoJson.features.map((feature) =>
+//       new mapboxgl.Marker()
+//         .setLngLat(feature.geometry.coordinates)
+//         .addTo(map)
+//     );
+
+//     // Add navigation control (the +/- zoom buttons)
+//     map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+//     // Clean up on unmount
+//     return () => map.remove();
+//   }, []);
+
+
+
+
+//   return (
+//     <div>
+//       <h1>MapS!!</h1>
+//       {/* Gives the map with the inital coordinates */}
+//       {/* <div className="sidebar">Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}</div> */}
+//       <div ref={mapContainer} className="map-container" />
+
+//       {/* not sure if we need the next line, used for showing lat/long on screen */}
+//     </div>
+//   )
+// }
+
+// export default Map
+
 }
-
-export default Map

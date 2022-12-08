1da81dbbb17bbef1e959b4f7c63a5f0c68348350
vrkljam <vrkljam@earthlink.net>
2022-12-07 19:43:25 -0700

    updates to show page

--- src/components/ShowPage.jsx
+++ src/components/ShowPage.jsx
@@ -4,9 +4,9 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import { useGlobalContext } from '../context/shelterContext'
 import {useParams} from 'react-router-dom'
 import { BsFillTelephoneFill } from 'react-icons/bs';
-import { BsFillEnvelopeFill } from 'react-icons/bs';
 import { BiEnvelope } from "react-icons/bi";
 import { CgWebsite } from "react-icons/cg";
+import Carousel from 'react-bootstrap/Carousel';
 
 
 
@@ -19,13 +19,13 @@ function ShowPage() {
 
     
   return (
-    <div>ShowPage
-        <Card style={{ width: '40rem' }}>
-        <Card.Header>Featured</Card.Header>
+    <div>
+        <Card style={{ width: '30rem' }}>
+        <Card.Header></Card.Header>
         <ListGroup variant="flush">
             <h1><ListGroup.Item className='shelter-name'>{theShelter.name}</ListGroup.Item></h1>
             <h3><ListGroup.Item>{theShelter.street},{theShelter.city},{theShelter.state},{theShelter.postalcode}</ListGroup.Item></h3>
-            <h4><i class="bi bi-telephone-fill"> {(theShelter.phone)? <ListGroup.Item ><BsFillTelephoneFill />{theShelter.phone}</ListGroup.Item>:<ListGroup.Item><BsFillTelephoneFill />  Phone not listed.</ListGroup.Item>}</i></h4>
+            <h4>{(theShelter.phone)? <ListGroup.Item ><BsFillTelephoneFill />{theShelter.phone}</ListGroup.Item>:<ListGroup.Item><BsFillTelephoneFill />  Phone not listed.</ListGroup.Item>}</h4>
             
             <h4><ListGroup.Item><CgWebsite /><a href={theShelter.url} target="_blank">{theShelter.url}</a></ListGroup.Item></h4>
             <ListGroup.Item>Services: {theShelter.services}</ListGroup.Item>

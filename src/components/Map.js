3f10fdd8eeddb69069690bd45418efed3dc34814
vrkljam <vrkljam@earthlink.net>
2022-12-08 13:32:21 -0700

    eslint on map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect, useState} from 'react';
-// import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+// import mapboxgl from 'mapbox-gl';
 import { useGlobalContext } from '../context/shelterContext'
 
 // Mapbox key

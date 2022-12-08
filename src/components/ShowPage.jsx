b5c7309718100eace184606015b43e148dce871e
vrkljam <vrkljam@earthlink.net>
2022-12-08 00:08:17 -0700

    show page css update

--- src/components/ShowPage.jsx
+++ src/components/ShowPage.jsx
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { useGlobalContext } from '../context/shelterContext'
 import {useParams} from 'react-router-dom'
-import { BsFillTelephoneFill } from 'react-icons/bs';
+import { BsFillTelephoneFill, BsArrowRightCircle } from 'react-icons/bs';
 import { BiEnvelope } from "react-icons/bi";
 import { CgWebsite } from "react-icons/cg";
 import Carousel from 'react-bootstrap/Carousel';
@@ -20,19 +20,21 @@ function ShowPage() {
     
   return (
     <div>
-        <Card style={{ width: '30rem' }}>
-        <Card.Header></Card.Header>
+        <Card className="mt-3" style={{ width: '30rem' }}>
+        {/* <Card.Header></Card.Header> */}
         <ListGroup variant="flush">
-            <h1><ListGroup.Item className='shelter-name'>{theShelter.name}</ListGroup.Item></h1>
-            <h3><ListGroup.Item>{theShelter.street},{theShelter.city},{theShelter.state},{theShelter.postalcode}</ListGroup.Item></h3>
-            <h4>{(theShelter.phone)? <ListGroup.Item ><BsFillTelephoneFill />{theShelter.phone}</ListGroup.Item>:<ListGroup.Item><BsFillTelephoneFill />  Phone not listed.</ListGroup.Item>}</h4>
+            {/* <h2><ListGroup.Item className='shelter-name'>{theShelter.name}</ListGroup.Item></h2> */}
+            <h2 className='mt-3 ms-3 shelter-name'>{theShelter.name}</h2>
+            <h4 className='mt-3 ms-3'>{theShelter.street},{theShelter.city},{theShelter.state},{theShelter.postalcode}</h4>
+            <span>{(theShelter.phone)? <p className='mt-3 ms-3'><BsFillTelephoneFill />{theShelter.phone}</p>:<p className='mt-3 ms-3'><BsFillTelephoneFill />  Phone not listed.</p>}</span>
+         
+            <p className='m-3 p-0'>Services: {theShelter.services}</p>
+      
+            <p className='ms-3 m-0 p-0'><CgWebsite /><a href={theShelter.url} target="_blank">{theShelter.url}</a></p>
+            <p className='mt-0 m-3 p-0'><BiEnvelope />  {theShelter.email}</p>
             
-            <h4><ListGroup.Item><CgWebsite /><a href={theShelter.url} target="_blank">{theShelter.url}</a></ListGroup.Item></h4>
-            <ListGroup.Item>Services: {theShelter.services}</ListGroup.Item>
-            <ListGroup.Item><BiEnvelope />  {theShelter.email}</ListGroup.Item>
-            {/* <ListGroup.Item><p>Interested in volunteering or would like to learn more?Fill out the form to apply now!</p></ListGroup.Item>
-            <button>Apply Now</button> */}
-
+            <p className='p-0 m-0 ms-3'>Interested in volunteering or would like to learn more?</p> <p className='p-0 m-0 ms-3'>Fill out the form to apply now!</p>
+            <button className='btn-donate ms-3 mt-1'><span id='donate-icon'></span>Apply Now <BsArrowRightCircle /></button>
 
         </ListGroup>
         </Card>
@@ -42,46 +44,3 @@ function ShowPage() {
 }
 
 export default ShowPage
-
-
-// function AnimalShelters() {
-
-// const {rescueGroupData, flyThis} = useGlobalContext()
-
-// if(!rescueGroupData) {
-//   return (
-//     <h4>Please enter a location to search.</h4>
-//   )
-// } else if (rescueGroupData.length < 1) {
-//   return (
-//     <h4>There were no shelters found within your search area.</h4>
-//   )
-// } else {  
-//     return (
-//       <>
-//         <div>Animal Shelter Info</div>
-//         <div>
-//           {rescueGroupData.map((singleGroup) => {
-//             return (
-//               <div key={singleGroup.attributes.id}>
-//                 <h4>{singleGroup.attributes.name}</h4>
-//                 <p>City: {singleGroup.attributes.citystate}</p> 
-//                 <p>Distance (from 90210 hardcoded): {singleGroup.attributes.distance} miles</p> 
-//                 <p>Email: {singleGroup.attributes.email}</p> 
-//                 <p>Phone: <a href={singleGroup.attributes.url} target="_blank">{singleGroup.attributes.url}</a></p> 
-//                 <p>Website: {singleGroup.attributes.phone}</p> 
-
-//                 <p>Services: {singleGroup.attributes.phone}</p> 
-//                 <button>{singleGroup.attributes.coordinates}</button>
-//                 <button >FLY</button>
-
-//               </div>
-//             )
-//           })}  
-//         </div>
-//       </>
-//     )
-//   } 
-// }
-
-// export default AnimalShelters

5f0ccf09f456c1ca335682c916f94f39e68cdb30
faizamahmoud <faiza.mahmoud@gmail.com>
2022-12-07 21:58:11 -0700

    added searchbar2

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import SearchResult from './pages/SearchResult';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import ShowPage from './components/ShowPage';
-
+import SearchBar2 from './components/SearchBar2/SearchBar2';
 function App() {
-  return (    
-    <div className="App">   
-      <Navbar />       
+  return (
+    <div className="App">
+      <Navbar />
       <Routes>
+        <Route path='/hi' element={<SearchBar2 />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/searchResults" element={<SearchResult />} /> 
+        <Route path="/searchResults" element={<SearchResult />} />
         <Route path="/show/:id" element={<ShowPage />} />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>

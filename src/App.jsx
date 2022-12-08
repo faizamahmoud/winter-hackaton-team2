3b7ed0163a5fe6b14c778e867f2989bbf7905bb4
rpeterson87 <oshorhino5053@gmail.com>
2022-12-07 21:32:55 -0500

    remove a few more things from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import LandingPage from './pages/landingPage';
-import AnimalShelters from './components/AnimalShelters';
 import SearchResult from './pages/SearchResult';
 import Search from './components/Search';
 
